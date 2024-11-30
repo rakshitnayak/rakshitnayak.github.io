2fe0ab57009a1a835dfd59e7305ddb18d0e8d860
rakshitnayak <rdsonnn@gmail.com>
2024-11-30 20:08:16 +0530

    update resume

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -9,7 +9,7 @@ function Button() {
   return (
     <button
       type="button"
-      onClick={() => router.push("https://drive.google.com/file/d/1Dh_VJiZBXPRqm0j5ivw0KYlwFZc_5d-L/view?usp=sharing")}
+      onClick={() => router.push("https://drive.google.com/file/d/12-0D3kFLEiKMW5ZPB40iitQW33236mm8/view?usp=sharing")}
       className="bg-[#6e57e0] hover:-translate-y-1 hover:scale-110 duration-300 rounded-full px-4 py-2 text-sm text-white mt-4 w-[100px]"
     >
       Resume
